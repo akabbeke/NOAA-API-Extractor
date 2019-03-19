61b9e96e87fbce958dec6e72ae55acd14ba39c8b
adam <akabbeke@gmail.com>
2019-03-19 15:06:44 -0700

    add python files

---
+++ extractor.py
@@ -0,0 +1,59 @@
+import json
+
+from datetime import date
+from datetime import timedelta
+from os import path
+
+import config
+
+from noaa_api import NOAADataset
+
+class Extractor(object):
+  def __init__(self, datesetid):
+    self.dataset = NOAADataset(datesetid)
+
+  def extract_date_range(self, start_date, end_date):
+    """ Extracts NOAA dataset for each day in an inclusive range of dates and
+    writes the data to JSON files.
+
+    :param start_date: The first date to be extracted
+    :param end_date: The last date to be extracted
+    """
+
+    # For each day in the range extract the data for that date
+    for i in range((end_date - start_date).days + 1):
+      self._extract_date(start_date + timedelta(days=i))
+
+  def _extract_date(self, date_to_fetch):
+    data = []
+    file_count = 0
+
+    # Stream the data in chunks
+    for data_chunk in self.dataset.stream_date_data(date_to_fetch):
+      # Add the current chunk of data to the batch of data
+      data += data_chunk
+
+      # If the current batch of data is larger than our file_line_limit
+      # then write it out
+      if len(data) >= config.file_line_limit:
+        for i in range(len(data)//config.file_line_limit):
+          self._write_file(date_to_fetch, file_count, data[:config.file_line_limit])
+          file_count += 1
+
+          # remove the data written out from the data list
+          data = data[config.file_line_limit:]
+
+    # write out an remainder data
+    self._write_file(date_to_fetch, file_count, data)
+
+  def _write_file(self, date_to_fetch, count, date_data):
+    # Writes out a file to the output_dir configured in the configs
+    # Filename is of the format "YYYY-MM-DD-0.json"
+
+    # Format the filename and path
+    filename = "{}-{}.json".format(date_to_fetch, count)
+    filepath = path.join(config.output_dir, filename)
+
+    # Write out the file in json format
+    with open(filepath, 'w') as output_file:
+      json.dump(date_data, output_file, indent=4)
---
+++ noaa_api.py
@@ -0,0 +1,92 @@
+import functools
+import logging
+import requests
+import urllib
+
+from datetime import date
+
+import config
+
+
+BASE_URL = 'https://www.ncdc.noaa.gov/cdo-web/api/v2/'
+
+
+class RetryLimit(Exception):
+  pass
+
+
+def retry_wrapper(func):
+  @functools.wraps(func)
+  def wrapper(*args, **kwargs):
+    for i in range(config.retry_limit):
+      request = func(*args, **kwargs)
+      print(request.status_code)
+      if request.status_code == requests.codes.ok:
+        return request
+    raise RetryLimit()
+  return wrapper
+
+
+class NOAADataset(object):
+  def __init__(self, datasetid):
+    self.datasetid = datasetid
+
+  def stream_date_data(self, date_to_fetch):
+    """Stream the data from the NOAA API for the date in chunks.
+
+    :param date_to_fetch: The date to fetch data for
+    :returns: A generator for fetching the chunks of data for the date
+    """
+
+    return self.stream_date_range_data(date_to_fetch, date_to_fetch)
+
+  def stream_date_range_data(self, start_date, end_date):
+    """Stream the data from the NOAA API for the days from the
+    start_date to the end_date in chunks.
+
+    :param date_to_fetch: The date to fetch data for
+    :returns: A generator for fetching the chunks of data in the date range
+    """
+
+    params = {'startdate': str(start_date), 'enddate': str(end_date)}
+    return self._stream_dataset(**params)
+
+  def _stream_dataset(self, **kwargs):
+    # Request the first set of data to get the metadata on the size of
+    # the entire dataset
+    request_data = self._make_request(**kwargs, offset=1).json()
+
+    # Some dates return no data. If this is the case return an empty list.
+    if not request_data:
+      return
+
+    # Parse the total count for the entire dataset
+    data_count = request_data['metadata']['resultset']['count']
+
+    # Yield the first set of data
+    yield request_data['results']
+
+    # Determine how many subsequent requests we will have to make
+    total_requests = int(data_count/config.request_limit) + 1
+
+    for i in range(1, total_requests):
+      # Make the request with the correct offset and then yield the result
+      request_data = self._make_request(**kwargs, offset=1 + i * config.request_limit)
+      yield request_data.json()['results']
+  
+  @retry_wrapper
+  def _make_request(self, **kwargs):
+    # Set the datasetid and limit 
+    kwargs.update({
+      'datasetid': self.datasetid,
+      'limit': config.request_limit,
+    })
+
+    # Generate the url for the request
+    url = "{}/{}".format(config.base_url, config.data_endpoint)
+
+    # Add the auth token to the request headers
+    headers = {'token': config.auth_token}
+
+    # return the request object
+    return requests.get(url, params=kwargs, headers=headers)
